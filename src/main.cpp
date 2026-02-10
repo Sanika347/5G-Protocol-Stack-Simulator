d650816c4f612c6e2f001197940ba111f4e9dcd7
Sanika347 <122077194+Sanika347@users.noreply.github.com>
2026-02-10 12:19:15 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,16 @@
+#include "../include/ue.h"
+#include <iostream>
+
+int main() {
+
+    UE userEquipment;
+
+    userEquipment.powerOn();
+
+    userEquipment.registerNetwork();
+
+    userEquipment.sendData("Hello 5G Network!");
+
+    return 0;
+}
 
