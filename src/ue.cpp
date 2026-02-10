3227181b7e868b6650038fcdfcc4c8b5d2f04640
Sanika347 <122077194+Sanika347@users.noreply.github.com>
2026-02-10 12:20:27 +0530

    Update ue.cpp

--- src/ue.cpp
+++ src/ue.cpp
@@ -1,1 +1,27 @@
+#include "../include/ue.h"
+#include <iostream>
+
+UE::UE() {}
+
+void UE::powerOn() {
+    std::cout << "\nUE Powered ON\n";
+}
+
+void UE::registerNetwork() {
+    nas.authenticate();
+    rrc.setupConnection();
+    std::cout << "UE Registered Successfully\n\n";
+}
+
+void UE::sendData(std::string data) {
+
+    std::cout << "Sending Data: " << data << "\n\n";
+
+    pdcp.encrypt(data);
+    rlc.segment(data);
+    mac.process(data);
+    phy.transmit(data);
+
+    std::cout << "\nData Sent Successfully\n";
+}
 
